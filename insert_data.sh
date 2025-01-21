16465acdaade97456bbec7ab577b101d173adfeb
Mark Anthony <senormarkanthonykamau@gmail.com>
2025-01-21 09:03:02 +0000

    Save progress: 810.1

--- insert_data.sh
+++ insert_data.sh
@@ -15,6 +15,10 @@ if [[ $MAJOR != major ]]
     then
   # insert major
       INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
+      if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
+        then
+          echo "Inserted into majors, $MAJOR"
+      fi
   # get new major_id
     fi
 
